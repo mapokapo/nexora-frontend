339664dce85aff7c39fc60fc279aa464c3439c40
mapokapo <leopetrovic11@gmail.com>
2025-03-15 15:34:27 +0100

    deps: add support for using SVG in React components

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 const dirname = import.meta.dirname;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(dirname, "./src"),
