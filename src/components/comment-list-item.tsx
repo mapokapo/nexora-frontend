5ceaa5180a5aa184a24a7141a465627b438b4167
mapokapo <leopetrovic11@gmail.com>
2025-03-17 16:48:40 +0100

    feat: add support for comment deletion

--- src/components/comment-list-item.tsx
+++ src/components/comment-list-item.tsx
@@ -1,17 +1,29 @@
 import { Loading } from "@/components/loading";
 import ProfilePicture from "@/components/profile-picture";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { firestore } from "@/lib/firebase";
+import { useAppUser } from "@/lib/hooks/use-user";
 import AsyncValue from "@/lib/types/AsyncValue";
 import { Comment } from "@/lib/types/Comment";
 import { Profile, profileSchema } from "@/lib/types/Profile";
-import { doc, onSnapshot } from "firebase/firestore";
+import { mapError } from "@/lib/utils";
+import { deleteDoc, doc, onSnapshot } from "firebase/firestore";
+import { Ellipsis, Trash } from "lucide-react";
 import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 interface CommentListItemProps {
   comment: Comment;
 }
 
 const CommentListItem: React.FC<CommentListItemProps> = ({ comment }) => {
+  const user = useAppUser();
   const [commenter, setCommenter] = useState<AsyncValue<Profile>>({
     loaded: false,
   });
@@ -37,6 +49,15 @@ const CommentListItem: React.FC<CommentListItemProps> = ({ comment }) => {
     });
   });
 
+  const onDelete = async () => {
+    try {
+      await deleteDoc(doc(firestore, "comments", comment.id));
+    } catch (error) {
+      const message = mapError(error);
+      toast.error(message);
+    }
+  };
+
   return (
     <li className="flex items-center gap-2 rounded-lg border border-border p-2 hover:bg-muted">
       <div className="h-6 w-6">
@@ -50,6 +71,28 @@ const CommentListItem: React.FC<CommentListItemProps> = ({ comment }) => {
         )}
       </div>
       <span>{comment.content}</span>
+      {comment.userId === user.uid && (
+        <div className="ml-auto">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                className="h-auto w-auto p-1"
+                variant="ghost"
+                size="icon">
+                <Ellipsis size={24} />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem
+                className="flex items-center text-destructive"
+                onClick={onDelete}>
+                <Trash />
+                <span>Delete</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      )}
     </li>
   );
 };
