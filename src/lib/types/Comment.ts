d99d2237585f86ab36673127273b356b1e4fea14
mapokapo <leopetrovic11@gmail.com>
2025-04-08 17:28:28 +0200

    fix: update comment schema to support firestore timestamps as well as string dates

--- src/lib/types/Comment.ts
+++ src/lib/types/Comment.ts
@@ -1,4 +1,5 @@
 import { firestoreTimestampSchema } from "@/lib/utils";
+import { Timestamp } from "firebase/firestore";
 import { z } from "zod";
 
 export const commentSchema = z.object({
@@ -6,8 +7,16 @@ export const commentSchema = z.object({
   content: z.string().min(1, "Content is required"),
   postId: z.string().min(1, "Post ID is required"),
   userId: z.string().min(1, "User ID is required"),
-  createdAt: firestoreTimestampSchema.transform(data => data.toDate()),
-  updatedAt: firestoreTimestampSchema.transform(data => data.toDate()),
+  createdAt: z
+    .union([firestoreTimestampSchema, z.string()])
+    .transform(data =>
+      data instanceof Timestamp ? data.toDate() : new Date(data)
+    ),
+  updatedAt: z
+    .union([firestoreTimestampSchema, z.string()])
+    .transform(data =>
+      data instanceof Timestamp ? data.toDate() : new Date(data)
+    ),
 });
 
 export type Comment = z.infer<typeof commentSchema>;
